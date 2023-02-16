471644cf7961e37e2248b103df7674f8e3323bce
AbhiGowrav <107835806+AbhiGowrav@users.noreply.github.com>
2023-02-16 14:36:18 +0530

    Update setup2.ps1

--- Allfiles/10/setup2.ps1
+++ Allfiles/10/setup2.ps1
@@ -12,19 +12,13 @@ Install-Module -Name Az.Sql -Force -Verbose:$false -WarningAction 'SilentlyConti
 Install-Module -Name Az.Storage -Force -Verbose:$false -WarningAction 'SilentlyContinue'
 Install-Module -Name Az.Resources -Force -Verbose:$false -WarningAction 'SilentlyContinue'
 
-#Creating required variables
-
-$AzureUsername = Read-Host -Prompt "Enter your Azure account name"
-$AzurePassword = Read-Host -Prompt "Enter your Azure account password" -AsSecureString 
-$ResourceGroupName = Read-Host -Prompt "Enter your resource group name" 
-
 $AzCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($AzureUsername, $AzurePassword)
 Connect-AzAccount -Credential $AzCredential
 
 $Location = Get-AzResourceGroup -Name $ResourceGroupName | Select-Object -ExpandProperty location
 $storageName = ('dp500sa' + (Get-Random -Minimum 0 -Maximum 999999 ).ToString('000000')).ToLower()
 $ServerName = ('dp500server-' + (Get-Random -Minimum 0 -Maximum 999999 ).ToString('000000')).ToLower()
-$FolderName = "D:\DP500"
+$FolderName = "C:\LabFiles\dp500"
 
 #Create storage accountadmin
 
